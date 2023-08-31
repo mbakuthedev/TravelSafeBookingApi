1a5d5e755568cbdb25414b19c62721b9af2e6288
Adeogo favour <favourdamilareadeogo@gmail.com>
2023-08-31 10:59:25 +0100

    booking credentials created

---
+++ DataModels/BookingCredentials.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelSafeBookingApi.DataModels
+{
+    public class BookingCredentials : BaseDataModel
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TravellingFrom { get; set; }
+        public string TravellingTo { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Title { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public Gender Gender { get; set; }
+        public string Address { get; set; }
+        public string BusId { get; set; }
+
+    }
+}
