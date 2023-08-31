33e50853370c039f9e65f9ea876b5572ad7e7054
Adeogo favour <favourdamilareadeogo@gmail.com>
2023-08-31 22:03:59 +0100

    update of payment transactions

--- DataModels/PaymentTransactions.cs
+++ DataModels/PaymentTransactions.cs
@@ -4,13 +4,29 @@ namespace TravelSafeBookingApi.DataModels
 {
     public class PaymentTransactions : BaseDataModel
     {
+        /// <summary>
+        /// The customer id of the customer initiating the transaction
+        /// </summary>
         public string CustomerId { get; set; }
 
+        /// <summary>
+        /// The order id of the customer
+        /// </summary>
         public string OrderId { get; set; }
 
-        public int PaymentVerified { get; set; }
+        /// <summary>
+        /// The payment status of the customer
+        /// </summary>
+        public PaymentStatus PaymentStatus { get; set; }
 
+        /// <summary>
+        /// The payment reference id of the customer
+        /// </summary>
         public string ReferenceId { get; set; }
+
+        /// <summary>
+        /// The foreign Key of the customer
+        /// </summary>
 
         [ForeignKey(nameof(CustomerId))]
         public List<CustomerDataModel> Customers { get; set; }
