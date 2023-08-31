9ab219907d97b639392e2ed23ca38e939a6fdd6c
Adeogo favour <favourdamilareadeogo@gmail.com>
2023-08-31 22:05:50 +0100

    addition of new operations

--- DomainOperations/BookingOperations.cs
+++ DomainOperations/BookingOperations.cs
@@ -26,7 +26,7 @@ namespace TravelSafeBookingApi.DomainOperations
             {
                 //  Check if the credentials and the Customer data model are the same
                var customers = await _context.Customers.FirstOrDefaultAsync( x => x.Email == credentials.Email);
-                
+
                 // check if the customer is null
                 if (customers == null)
                 {
@@ -39,22 +39,20 @@ namespace TravelSafeBookingApi.DomainOperations
                         TravellingFrom = credentials.TravellingFrom,
                         TravellingTo = credentials.TravellingTo,
                         Phone = credentials.PhoneNumber,
-                        Gender = credentials.Gender,
                         Title = credentials.Title,
-                        ArrivalDate = credentials.ArrivalDate,
+                        DepatureDate = credentials.DepartureDate,
+                        BusId = credentials.BusId
                     };
-                     
+
                     // Add them to the database
                     await _context.Customers.AddAsync(newCustomers);
 
                     // Save the changes
                     _context.SaveChanges();
+                }
 
                     // Fetch the price of where the customer is going
                    var routePrices =  await _context.BusRoutes.FirstOrDefaultAsync(x => x.To == credentials.TravellingTo);
-
-                    // Fetch the bus 
-                    var bus = await _context.Buses.FirstOrDefaultAsync(x => x.Id == credentials.BusId);
                     
                     // check if the destination exists
                     if (routePrices == null)
@@ -65,13 +63,18 @@ namespace TravelSafeBookingApi.DomainOperations
                             Status = (int)HttpStatusCode.NotFound
                         };
                     }
+                    
+                    // create a reservation id
+                    var reservationId = Guid.NewGuid().ToString();
+
+                    // create a new booking id 
                     var newBookingOrder = new ReservationDataModel
                     {
                         Price = routePrices.Price,
                         PaymentStatus = PaymentStatus.Pending,
                         CustomerId = customers.Id,
-                        ReservationId = Guid.NewGuid().ToString(),
-                        BusId = bus.Id
+                        ReservationId = reservationId,
+                        BusId = credentials.BusId
                     };
 
                     // add the changes to db
@@ -79,12 +82,11 @@ namespace TravelSafeBookingApi.DomainOperations
 
                     // save the changes
                    await _context.SaveChangesAsync();
-
-                   
-                }
+                    
+                // return the result
                 result = new OperationResult
                 {
-                   // Result = 
+                   Result = reservationId
                 };
 
             }
@@ -103,6 +105,67 @@ namespace TravelSafeBookingApi.DomainOperations
             }
             // return the result
             return result;
+        }
+
+        public async Task<OperationResult> ReserveBus(string reservationId)
+        {
+            // Initialize operation result
+            OperationResult result;
+
+            try
+            {
+              var custReservation = await _context.Reservations.FirstOrDefaultAsync(x => x.ReservationId == reservationId);
+                if (custReservation == null)
+                {
+                    result = new OperationResult
+                    {
+                        ErrorMessage = "The reservation Id does not exist",
+                        Status = (int)HttpStatusCode.NotFound
+                    };
+                }
+                else
+                {
+                    // update the reservations
+                    var newReservationDetails = new ReservationDataModel
+                    {
+                        PaymentStatus = PaymentStatus.Initialized,
+                    };
+
+                    // update the reservation details
+                    _context.Reservations.Update(newReservationDetails);
+
+                    // initialize payments transactions
+                    var payments = new PaymentTransactions
+                    {
+                        CustomerId = custReservation.CustomerId,
+                        OrderId = custReservation.ReservationId,
+                        //ReferenceId = Guid.NewGuid().ToString(),
+                        PaymentStatus = PaymentStatus.Initialized
+                    };
+
+                    // commit the changes to the database
+                    _context.Payments.Update(payments);
+
+                    // update the database
+                    _context.SaveChanges();
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                _logger.LogError(message: ex.Message, ex);
+
+                // Return error result
+                result = new OperationResult
+                {
+                    ErrorTitle = "SYSTEM ERROR",
+                    ErrorMessage = "Transaction could not be initiated",
+                    Status = (int)HttpStatusCode.InternalServerError
+                };
+            }
         }
     }
 }
